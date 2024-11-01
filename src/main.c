ed85eaef3633c544da45e8e84485d34532df7a43
Archie Beales <archiebeales@gmail.com>
2024-11-01 18:11:37 +0100

    added null terminator to token list to help with printing

--- src/main.c
+++ src/main.c
@@ -5,20 +5,24 @@
 #include<stdlib.h>
 
 
-int print_tokens(char** tokens, char* input){
+// TODO: DEBUGGING FUNCTION TO BE REMOVED LATER 
+// on peut garder la fonction si on enleve le printf et on utiliser notre fonction IO
+void print_tokens(char** tokens){
   // on suppose que tokens est une liste valid de strings
-  for(int i = 0 ; i < len_tokens(input); i++){
+  for(int i = 0 ; tokens[i] != NULL; i++){
     printf("token %d : %s    ",i, tokens[i]);
   } 
   printf("\n");
 }
 
 int main(){
-  int result;
   char* input = malloc(1000*sizeof(char)); // TODO: the max size should be changed later  
   while (1){
-    result = print_prompt(1); // storing the result for potential debugging later on
+    if ( print_prompt(1) == -1){
     // error handling
+      perror("error printing prompt in main.c");
+      return -1;
+    };
     if(input == NULL){
       perror("error allocating space in main.c");
       return -1;
@@ -26,7 +30,8 @@ int main(){
     fgets(input, 1000* sizeof(char), stdin);
     // tokenise the input
     char** tokens = tokenise(input);
-    print_tokens(tokens, input);
+    print_tokens(tokens);
+
     if (strcmp(tokens[0], "exit") == 0){
       // break out of the loop
       return 0;
