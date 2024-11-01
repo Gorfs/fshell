5cbca247a4b80195ae3c340f2c8d023630213bbf
Archie Beales <archiebeales@gmail.com>
2024-11-01 12:53:46 +0100

    fixed problem with prompt.h having duplicates of prompt_max_length

--- include/prompt.h
+++ include/prompt.h
@@ -2,7 +2,6 @@
 #define PROMPT_H
 
 extern const char* prompt_suffix;
-int print_prompt(int file_descriptor);
-int prompt_MAX_SIZE = 1000; // TODO convert this to the number that is 1000 * sizeof(char)
+extern int print_prompt(int file_descriptor);
 
 #endif
