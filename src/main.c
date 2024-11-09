5ff61e09d6164ad489a51ea680631665513ec3da
Christophe <christophejin2004@gmail.com>
2024-11-09 15:35:40 +0100

    Basic version of the command 'pwd' has been added to fshell.

--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <string.h>
-#include<prompt.h>
+#include <stdlib.h>
+#include <unistd.h>
+// personal files includes
+#include <prompt.h>
 #include <tokenisation.h>
-#include<stdlib.h>
-
+#include <pwd.h>
 
 // TODO: DEBUGGING FUNCTION TO BE REMOVED LATER 
 // on peut garder la fonction si on enleve le printf et on utiliser notre fonction IO
@@ -17,12 +19,13 @@ void print_tokens(char** tokens){
 
 int main(){
   char* input = malloc(PROMPT_MAX_SIZE*sizeof(char)); // TODO: the max size should be changed later  
-  while (1){
+  while (1)
+  {
     if ( print_prompt(1) == -1){
-    // error handling
+      // error handling
       perror("error printing prompt in main.c");
       return -1;
-    };
+    }
     if(input == NULL){
       perror("error allocating space in main.c");
       return -1;
@@ -31,6 +34,15 @@ int main(){
     // tokenise the input
     char** tokens = tokenise(input);
     print_tokens(tokens);
+
+    if (strcmp(tokens[0], "pwd") == 0) // Check if the first token is "pwd"
+    {
+      if (command_pwd(1, tokens) == -1)
+      {
+        perror("error executing pwd command in main.c");
+        return -1;
+      }
+    }
 
     if (strcmp(tokens[0], "exit") == 0){
       // break out of the loop
