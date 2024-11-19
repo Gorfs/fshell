6219c0d2f6672eaeada3d0744180acbdfb02d032
Christophe <christophejin2004@gmail.com>
2024-11-19 19:05:11 +0100

    Removed 1 to every text.

--- src/ftype.c
+++ src/ftype.c
@@ -8,23 +8,23 @@ int command_ftype(char** command){
     struct stat st;
     if(lstat(command[1], &st) == 0){
         if(S_ISLNK(st.st_mode)){
-            write(1, "symbolic link\n", 15);
+            write(1, "symbolic link\n", 14);
         }
         else if(S_ISDIR(st.st_mode)){
-            write(1, "directory\n", 11);
+            write(1, "directory\n", 10);
         }
         else if(S_ISREG(st.st_mode)){
-            write(1, "regular file\n", 14);
+            write(1, "regular file\n", 13);
         }
         else if(S_ISFIFO(st.st_mode)){
-            write(1, "named pipe\n", 12);
+            write(1, "named pipe\n", 11);
         }
         else{
-            write(1, "other\n", 7);
+            write(1, "other\n", 6);
         }
     }
     else{
-        write(STDERR_FILENO, "ftype: No such file or directory\n", 34);
+        write(STDERR_FILENO, "ftype: No such file or directory\n", 33);
         return 1;
     }
     return 0;
